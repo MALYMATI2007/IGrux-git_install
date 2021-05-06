cf6c729deb0dad0052396b49b0ebe95f680b172b
MALYMATI2007 <75562552+MALYMATI2007@users.noreply.github.com>
2021-05-06 21:16:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,61 @@
 import time
-import colorama
 from colorama import init, Fore
 import webbrowser
 import os
 import hashlib
 from audioplayer import AudioPlayer
+import ctypes
 
 init(autoreset=True)
+ctypes.windll.kernel32.SetConsoleTitleW("IGrux")
 
 def haslo(a):
     b = a.encode()
     c = hashlib.sha256(b)
     if c.hexdigest() == 'ea3a03b4971eeb62730e1de238225cc4e6145f0eb50ad28b1379f2a2ee71e16e':
         time.sleep(3)
-        print(Fore.GREEN + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nDOSTĘP PRZYZNANO')
+        os.system('cls')
+        print(Fore.GREEN + 'DOSTĘP PRZYZNANO')
         dostep()
     else:
-        print(Fore.RED + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nODMOWA DOSTĘPU')
+        os.system('cls')
+        print(Fore.RED + 'ODMOWA DOSTĘPU')
         print(Fore.CYAN + 'Podaj Hasło:')
         Pass = input('>')
         haslo(Pass)
 
 def dostep():
-    print(Fore.CYAN + '1 - Web\n2 - Gry\n3 - Passwords\n4 - Notatki\n5 - Play Audio\n6 - Exit')
+    print(Fore.CYAN + '1 - Web\n2 - Gry\n3 - Produkty w Sklepie u MrEsxej`a\n4 - Notatki\n5 - Play Audio\n6 - Piwnica\n7 - Autorzy\n8 - Ustawienia\n9 - Exit')
     button = input('>')
     if button == '1':
-        print(Fore.CYAN + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nWpisz URL lub napisz quit aby wyjść:')
+        os.system('cls')
+        print(Fore.CYAN + 'Wpisz URL lub napisz quit aby wyjść:')
         url = input('>')
         if url == 'quit':
-            print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+            os.system('cls')
             dostep()
         else:
             webbrowser.open(url)
             Back()
     elif button == '2':
-        print(Fore.CYAN + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n0 Gier')
+        os.system('cls')
+        print(Fore.CYAN + 'Gry:')
+        path = 'Games/'
+
+        files = os.listdir(path)
+
+        for f in files:
+            print(f)
+        game_path = input('>')
+        os.startfile(f'Games\{game_path}')
         Back()
     elif button == '3':
-        print(Fore.CYAN + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHasła:\nABC\n1234567\nQASDER')
+        os.system('cls')
+        print(Fore.CYAN + 'Produkty w Sklepie u MrEsxej`a:\nGruz\nPiwniczanka\nTynkErs\nTynkośniki\nTynkeppers\nKinder tynkulada\nJajko gruzanko\nTynkiWay\nSpagetti z kabalbali\nTynk-Tak\nLimpton Gruz Tea')
         Back()
     elif button == '4':
-        print(Fore.CYAN + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nNotatki:')
+        os.system('cls')
+        print(Fore.CYAN + 'Notatki:')
         path = 'Notes/'
 
         files = os.listdir(path)
@@ -59,7 +74,8 @@ def dostep():
 
         Back()
     elif button == '5':
-        print(Fore.CYAN + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\naudio:')
+        os.system('cls')
+        print(Fore.CYAN + 'audio:')
         path = 'Audio/'
         files = os.listdir(path)
         for f in files:
@@ -68,45 +84,61 @@ def dostep():
         AudioPlayer('Audio/' + sound).play(block=True)
         Back()
     elif button == '6':
+        os.system('cls')
+        print('Zapraszamy!')
+        webbrowser.open('https://discord.gg/uhAQgTF')
+        Back()
+    elif button == '7':
+        os.system('cls')
+        print(Fore.CYAN + 'AUTORZY:\n\nZaprogramował:\nMALYMATI2007\n\nUlepszył drobiazgami:\nKoliw\n\nNazwę wymyślił:\nPabloss')
+        Back()
+    elif button == '8':
+        os.system('cls')
+        print('COMMING SOON')
+        Back()
+    elif button == '9':
         print(Fore.RED + 'DOWIDZENIA!')
         time.sleep(1)
         exit()
     else:
-        print(Fore.RED + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nERROR')
+        os.system('cls')
+        print(Fore.RED + 'ERROR')
         dostep()
 
 def Back():
     print(Fore.CYAN + '\n1 - Wyjdź')
     backo = input('>')
     if backo == '1':
-        print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        os.system('cls')
         dostep()
     else:
-        print(Fore.RED + '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nERROR')
+        os.system('cls')
+        print(Fore.RED + 'ERROR')
         Back()
 
 def Animation():
+    Anim_Frame('I', '')
+    Anim_Frame('IG', '')
+    Anim_Frame('IGr', '')
+    Anim_Frame('IGru', '')
+    Anim_Frame('IGrux', '')
+    Anim_Frame('IGrux', 'Loading')
+    Anim_Frame('IGrux', 'Loading.')
+    Anim_Frame('IGrux', 'Loading..')
+    Anim_Frame('IGrux', 'Loading...')
+    Anim_Frame('IGrux', 'Loaded!')
+
+def Anim_Frame(In_Frame1, In_Frame2):
+    os.system('cls')
     a = Fore.GREEN
-    space = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
-    print(space + a + '========================\nI\n\n========================')
+    print(a + f'========================\n{In_Frame1}\n{In_Frame2}\n========================')
     time.sleep(1)
-    print(space + a + '========================\nIG\n\n========================')
-    time.sleep(1)
-    print(space + a + '========================\nIGr\n\n========================')
-    time.sleep(1)
-    print(space + a + '========================\nIGru\n\n========================')
-    time.sleep(1)
-    print(space + a + '========================\nIGrux\n\n========================')
-    time.sleep(1)
-    print(space + a + '========================\nIGrux\nLoading\n========================')
-    time.sleep(1)
-    print(space + a + '========================\nIGrux\nLoading.\n========================')
-    time.sleep(1)
-    print(space + a + '========================\nIGrux\nLoading..\n========================')
-    time.sleep(1)
-    print(space + a + '========================\nIGrux\nLoading...\n========================')
-    time.sleep(1)
-    print(space + a + '========================\nIGrux\nLoaded!\n========================')
+
+#client_id = "834734074014203905"
+#RPC = Presence(client_id)
+#RPC.connect()
+
+#print(RPC.update(state="GitHub", buttons=[{"label":"IGruX", "url":"https://github.com/MALYMATI2007/IGrux-git_install/blob/main/main.py"}]))  # Set the presence
 
 Animation()
 print(Fore.CYAN + 'Podaj Hasło:')
